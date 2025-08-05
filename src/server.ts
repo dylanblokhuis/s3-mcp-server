710be345fef82b2f2ea63dbc13d219c90d768d5a
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2025-08-05 15:34:15 +0200

    S3 endpoint

--- src/server.ts
+++ src/server.ts
@@ -137,6 +137,7 @@ const s3Client = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
+  endpoint: process.env.S3_ENDPOINT || undefined,
 });
 
 const BUCKET_NAME = process.env.S3_BUCKET_NAME || "";
